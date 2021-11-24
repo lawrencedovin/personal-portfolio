a6c95b0786b63b6bcdad0afeb780b38be3e866e1
Lawrence Dovin <lawrencedovin@gmail.com>
2021-11-24 13:13:18 -0500

    Updated navbar styles

--- src/components/nav/NavBar/NavBar.js
+++ src/components/nav/NavBar/NavBar.js
@@ -8,14 +8,13 @@ function NavBar() {
       <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
           <Link className="navbar-brand navbar__logo" to="/">
-            logo
             {/* <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" /> */}
+            <img src={process.env.PUBLIC_URL + 'icons/portfolio-icon.svg'} className="navbar__icon--portfolio" alt="github"/>
           </Link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <div class="me-auto"></div>
             <div class="d-flex">
                 <ul class="navbar-nav me-2">
                     <li class="nav-item">
@@ -29,6 +28,23 @@ function NavBar() {
                     </li>
                     <li class="nav-item">
                       <Link className="nav-link navbar__link" to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+            <div class="me-auto"></div>
+            <div class="d-flex">
+                <ul class="navbar-nav me-2">
+                    <li class="nav-item">
+                      <Link className="nav-link navbar__social" to="/about">About</Link>
+                    </li>
+                    <li class="nav-item">
+                      <Link className="nav-link navbar__social" to="/resume">Resume</Link>
+                    </li>
+                    <li class="nav-item">
+                      <Link className="nav-link navbar__social" to="/portfolio">Portfolio</Link>
+                    </li>
+                    <li class="nav-item">
+                      <Link className="nav-link navbar__social" to="/contact">Contact</Link>
                     </li>
                 </ul>
             </div>

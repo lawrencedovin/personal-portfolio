df423acbd249b7f5d0dae8aa7b40e97c194f3435
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-01 15:46:50 -0500

    Added contact button

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,14 +7,14 @@ function Hero() {
       <div className="hero__container--left">
         <div className="hero__details">
           <div className="hero__name">
-            Lawrence Dovin
+            LAWRENCE DOVIN
           </div>
           <div className="hero__underline--big"></div>
           <div className="hero__underline--small"></div>
           <div className="hero__title">
-            Software Engineer
+              Software Engineer
           </div>
-          <a href="/" className="hero__contact-btn">Contact Me</a>
+          <a href="/" className="hero__contact-btn btn"><span>Contact Me</span></a>
         </div>
       </div>
       <div className="hero__container--right">

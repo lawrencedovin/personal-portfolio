cf800fc895eaf8f38c87a9f236ed8cdf757cdb46
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-05 14:39:15 -0500

    Added preview container

---
+++ src/components/Preview/Preview.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function Preview() {
+  return (
+    <div className="row hero__container--preview">
+        <div className="col-md-4">sd</div>
+        <div className="col-md-4">sda</div>
+        <div className="col-md-4">ads</div>
+    </div>
+  );
+}
+
+Preview.propTypes = {};
+
+Preview.defaultProps = {};
+
+export default Preview;
--- src/components/main-pages/Home/Home.js
+++ src/components/main-pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from '../../Hero/Hero';
+import Preview from '../../Preview/Preview';
 
 
 function Home () {
   return(
     <div data-testid="Home">
       <Hero />
+      <Preview />
     </div>
   );
 }

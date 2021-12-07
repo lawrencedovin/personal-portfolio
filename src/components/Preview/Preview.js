f17a9adbd54ac8fb7453b1557b78ee69ea7bea75
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-07 17:50:52 -0500

    Updated preview buttons

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -6,19 +6,25 @@ function Preview() {
   return (
     <div className="row hero__container--preview">
         <div className="col-md-4 hero__item--preview">
-          <img src={process.env.PUBLIC_URL + 'logo192.png'} className="hero__image--preview" alt="mountains"/>
-          <button className="hero__button--preview hero__button--preview-about btn">ABOUT</button>
+          <div className="hero__image-container--preview">
+            <img src={process.env.PUBLIC_URL + 'logo192.png'} className="hero__image--preview" alt="mountains"/>
+          </div>
+          <a href="/" className="hero__button--preview hero__button--preview-about btn"><span>ABOUT</span></a>
         </div>
         
         <div className="col-md-4 hero__item--preview">
-          <img src={process.env.PUBLIC_URL + 'logo192.png'} className="hero__image--preview" alt="mountains"/>
-          <button className="hero__button--preview hero__button--preview-resume btn">RESUME</button>
+          <div className="hero__image-container--preview">
+            <img src={process.env.PUBLIC_URL + 'logo192.png'} className="hero__image--preview" alt="mountains"/>
+          </div>
+          <a href="/" className="hero__button--preview hero__button--preview-resume btn"><span>RESUME</span></a>
         </div>
 
         
         <div className="col-md-4 hero__item--preview">
-          <img src={process.env.PUBLIC_URL + 'logo192.png'} className="hero__image--preview" alt="mountains"/>
-          <button className="hero__button--preview hero__button--preview-portfolio btn">PORTFOLIO</button>
+          <div className="hero__image-container--preview">
+            <img src={process.env.PUBLIC_URL + 'logo192.png'} className="hero__image--preview" alt="mountains"/>
+          </div>
+          <a href="/" className="hero__button--preview hero__button--preview-portfolio btn"><span>PORTFOLIO</span></a>
         </div>
     </div>
   );

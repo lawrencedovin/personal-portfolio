38d28311c5fc56fd30b346fc66d06e35f35b8e7b
Lawrence Dovin <lawrencedovin@gmail.com>
2021-12-12 22:44:25 -0500

    updated contact info details

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,7 +5,16 @@ function ContactForm() {
   return(
     <div data-testid="ContactForm">
       <div className="contact__form-container">
-        <div className="contact__form--info"></div>
+        <div className="contact__form--info">
+          <h1 className="contact__form--info-title">
+            Contact
+          </h1>
+          <ul>
+            <li className="contact__form--info-details"><span><i className="fas fa-map-marker-alt"></i>Rockville, Maryland</span></li>
+            <li className="contact__form--info-details"><span><i className="fas fa-phone"></i>(610) 579-1888</span></li>
+            <li className="contact__form--info-details"><span><i className="fas fa-envelope"></i>lawrence.dovin@hotmail.com</span></li>
+          </ul>
+        </div>
         <form className="contact__form">
 
         </form>
